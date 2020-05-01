e5a3c2ba40140f93ed96f742f349e4db2d1aada6
Caspian Baska <caspianbaska@gmail.com>
2020-05-01 15:56:54 +1000

    test(loader): spec `PlaceOS::Frontends::Loader`

--- spec/helper.cr
+++ spec/helper.cr
@@ -1,2 +1,30 @@
 require "spec"
+
+require "file_utils"
+require "models/spec/generator"
+
 require "../src/frontends"
+
+TEST_DIR = "test-www"
+
+Spec.before_suite { reset }
+Spec.after_suite { reset }
+
+PlaceOS::Frontends::Loader.configure do |settings|
+  settings.content_directory = TEST_DIR
+end
+
+def reset
+  FileUtils.rm_rf(TEST_DIR)
+end
+
+def example_repository
+  existing = PlaceOS::Model::Repository.where(folder_name: "hello-world").first?
+  return existing if existing
+
+  repository = PlaceOS::Model::Generator.repository(type: PlaceOS::Model::Repository::Type::Interface)
+  repository.uri = "https://github.com/octocat/Hello-World"
+  repository.name = "hello-world"
+  repository.folder_name = "hello-world"
+  repository.save!
+end
--- spec/loader_spec.cr
+++ spec/loader_spec.cr
@@ -2,9 +2,37 @@ require "./helper"
 
 module PlaceOS::Frontends
   describe Loader do
-    # TODO: Write tests
-    it "works" do
-      false.should eq(true)
+    repository = example_repository
+    repository_folder_name = repository.folder_name.as(String)
+
+    Spec.before_each do
+      reset
+      repository = example_repository
+    end
+
+    it "loads frontends" do
+      loader = Loader.new.start
+
+      loader.processed.size.should eq 1
+
+      Dir.exists?(File.join(TEST_DIR, repository_folder_name)).should be_true
+
+      loader.stop
+    end
+
+    it "removes frontends" do
+      loader = Loader.new.start
+
+      loader.processed.size.should eq 1
+
+      Dir.exists?(File.join(TEST_DIR, repository_folder_name)).should be_true
+
+      repository.destroy
+      sleep 0.5
+
+      Dir.exists?(File.join(TEST_DIR, repository_folder_name)).should be_false
+
+      loader.stop
     end
   end
 end
