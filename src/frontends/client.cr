c29651f25dc25fbefdd3dc49cd67da32771fae9c
Caspian Baska <caspianbaska@gmail.com>
2020-05-04 12:11:45 +1000

    feat(client): simple client for frontends service

---
+++ src/frontends/client.cr
@@ -0,0 +1,174 @@
+require "http"
+require "json"
+require "mutex"
+require "uri"
+
+require "./constants"
+require "./error"
+
+module PlaceOS::Frontends
+  class Client
+    BASE_PATH   = "/api/frontends"
+    API_VERSION = "v1"
+    getter api_version : String
+
+    # Set the request_id on the client
+    property request_id : String?
+
+    getter host : String = ENV["PLACE_FRONTENDS_HOST"]? || "127.0.0.1"
+    getter port : Int32 = (ENV["PLACE_FRONTENDS_PORT"]? || 3000).to_i
+
+    # A one-shot Core client
+    def self.client(
+      uri : URI,
+      request_id : String? = nil,
+      api_version : String = API_VERSION
+    )
+      client = new(uri, request_id, api_version)
+      begin
+        response = yield client
+      ensure
+        client.connection.close
+      end
+
+      response
+    end
+
+    # Queries
+    ###########################################################################
+
+    # Returns the loaded repositories on the node
+    def loaded
+      response = get("/repositories")
+
+      Hash(String, String).from_json(response.body)
+    end
+
+    # Commits for a frontend folder
+    def commits(folder_name : String)
+      response = get("/repositories/#{folder_name}/commits")
+
+      Array(String).from_json(response.body)
+    end
+
+    ###########################################################################
+
+    def initialize(
+      uri : URI,
+      @request_id : String? = nil,
+      @api_version : String = API_VERSION
+    )
+      uri_host = uri.host
+      @host = uri_host if uri_host
+      @port = uri.port || 3000
+      @connection = HTTP::Client.new(uri)
+    end
+
+    def initialize(
+      host : String? = nil,
+      port : Int32? = nil,
+      @request_id : String? = nil,
+      @api_version : String = API_VERSION
+    )
+      @host = host if host
+      @port = port if port
+      @connection = HTTP::Client.new(host: @host, port: @port)
+    end
+
+    @connection : HTTP::Client?
+
+    protected def connection
+      @connection.as(HTTP::Client)
+    end
+
+    protected getter connection_lock : Mutex = Mutex.new
+
+    def close
+      connection_lock.synchronize do
+        connection.close
+      end
+    end
+
+    # Base struct for responses
+    private abstract struct BaseResponse
+      include JSON::Serializable
+    end
+
+    # API modem
+    ###########################################################################
+
+    {% for method in %w(get post) %}
+      # Executes a {{method.id.upcase}} request on core connection.
+      #
+      # The response status will be automatically checked and a `ClientError` raised if
+      # unsuccessful.
+      # ```
+      private def {{method.id}}(path, headers : HTTP::Headers? = nil, body : HTTP::Client::BodyType? = nil)
+        path = File.join(BASE_PATH, API_VERSION, path)
+
+        response = connection_lock.synchronize do
+          connection.{{method.id}}(path, headers, body)
+        end
+        raise ClientError.from_response("#{@host}:#{@port}#{path}", response) unless response.success?
+
+        response
+      end
+
+      # Executes a {{method.id.upcase}} request on the core client connection with a JSON body
+      # formed from the passed `NamedTuple`.
+      private def {{method.id}}(path, body : NamedTuple)
+        headers = HTTP::Headers{
+          "Content-Type" => "application/json"
+        }
+        headers["X-Request-ID"] = request_id unless request_id.nil?
+
+        {{method.id}}(path, headers, body.to_json)
+      end
+
+      # :ditto:
+      private def {{method.id}}(path, headers : HTTP::Headers, body : NamedTuple)
+        headers["Content-Type"] = "application/json"
+        headers["X-Request-ID"] = request_id unless request_id.nil?
+
+        {{method.id}}(path, headers, body.to_json)
+      end
+
+      # Executes a {{method.id.upcase}} request and yields a `HTTP::Client::Response`.
+      #
+      # When working with endpoint that provide stream responses these may be accessed as available
+      # by calling `#body_io` on the yielded response object.
+      #
+      # The response status will be automatically checked and a Core::ClientErrror raised if
+      # unsuccessful.
+      private def {{method.id}}(path, headers : HTTP::Headers? = nil, body : HTTP::Client::BodyType = nil)
+        connection.{{method.id}}(path, headers, body) do |response|
+          raise ClientError.from_response("#{@host}:#{@port}#{path}", response) unless response.success?
+          yield response
+        end
+      end
+
+      # Executes a {{method.id.upcase}} request on the core client connection with a JSON body
+      # formed from the passed `NamedTuple` and yields streamed response entries to the block.
+      private def {{method.id}}(path, body : NamedTuple)
+        headers = HTTP::Headers{
+          "Content-Type" => "application/json"
+        }
+        headers["X-Request-ID"] = request_id unless request_id.nil?
+
+        {{method.id}}(path, headers, body.to_json) do |response|
+          yield response
+        end
+      end
+
+      # :ditto:
+      private def {{method.id}}(path, headers : HTTP::Headers, body : NamedTuple)
+        headers["Content-Type"] = "application/json"
+        headers["X-Request-ID"] = request_id unless request_id.nil?
+
+        {{method.id}}(path, headers, body.to_json) do |response|
+          yield response
+        end
+      end
+    {% end %}
+  end
+end
---
+++ src/frontends/error.cr
@@ -0,0 +1,29 @@
+module PlaceOS::Frontends
+  class Error < Exception
+    getter message
+
+    def initialize(@message : String = "")
+      super(@message)
+    end
+  end
+
+  class ClientError < Error
+    getter status_code
+
+    def initialize(@status_code : Int32, message = "")
+      super(message)
+    end
+
+    def initialize(path : String, @status_code : Int32, message : String)
+      super("request to #{path} failed with #{message}")
+    end
+
+    def initialize(path : String, @status_code : Int32)
+      super("request to #{path} failed")
+    end
+
+    def self.from_response(path : String, response : HTTP::Client::Response)
+      new(path, response.status_code, response.body)
+    end
+  end
+end
